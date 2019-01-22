b00d561a4742d9c7ad8671b0fe407c85bdc1f824
freemmer-iMac <freemmer@gmail.com>
2019-01-22 10:52:22 +0900

    -Added demo code for logger and beanManager

--- app/src/main/java/com/tistory/freemmer/lib/libfm/demo/MainActivity.kt
+++ app/src/main/java/com/tistory/freemmer/lib/libfm/demo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.tistory.freemmer.lib.libfm.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.tistory.freemmer.lib.libfm.logger.FMILog
+import com.tistory.freemmer.lib.libfm.platform.FMBeanManager
 
 class MainActivity : AppCompatActivity() {
+
+    private val log = FMBeanManager.getClass(FMILog::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        log?.d("Hello world!")
     }
+
 }
