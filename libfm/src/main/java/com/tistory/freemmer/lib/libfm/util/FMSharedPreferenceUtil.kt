6ce3a222c079be7855d5a7a9acf3e08f6492fb80
freemmer-iMac <freemmer@gmail.com>
2019-02-01 15:56:28 +0900

    -Add 'FMSharedPreferenceUtil'

---
+++ libfm/src/main/java/com/tistory/freemmer/lib/libfm/util/FMSharedPreferenceUtil.kt
@@ -0,0 +1,75 @@
+package com.tistory.freemmer.lib.libfm.util
+
+import android.content.Context
+import android.preference.PreferenceManager
+import java.lang.ref.WeakReference
+
+/**
+ * Created by freemmer on 01/02/2019.
+ * History
+ *    - 01/02/2019 Create file
+ */
+class FMSharedPreferenceUtil private constructor(
+    val context: Context
+){
+
+    companion object {
+        private var weakReference: WeakReference<FMSharedPreferenceUtil>? = null
+        fun instance(context: Context): FMSharedPreferenceUtil {
+            if (weakReference?.get() == null) {
+                weakReference = WeakReference(FMSharedPreferenceUtil(context))
+            }
+            return weakReference?.get()!!
+        }
+    }
+
+    fun putSharedPreference(key: String, value: String?) {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = prefs.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun putSharedPreference(key: String, value: Boolean) {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = prefs.edit()
+        editor.putBoolean(key, value)
+        editor.apply()
+    }
+
+    fun putSharedPreference(key: String, value: Int) {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = prefs.edit()
+        editor.putInt(key, value)
+        editor.apply()
+    }
+
+    fun putSharedPreference(key: String, value: Float) {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = prefs.edit()
+        editor.putFloat(key, value)
+        editor.apply()
+    }
+
+    fun getSharedPreference(key: String): String? {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getString(key, null)
+    }
+
+    fun getBooleanSharedPreference(key: String): Boolean {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getBoolean(key, false)
+    }
+
+    fun getIntSharedPreference(key: String): Int {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getInt(key, -1)
+    }
+
+    fun getFloatSharedPreference(key: String): Float {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getFloat(key, -1f)
+    }
+
+}
+
